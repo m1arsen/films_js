0b94afb6e5ef6773c259d64c4478a776389ef685
m1arsen <m1arsen@mail.ru>
2022-09-28 13:48:44 +0300

    upgrade function

--- js/script.js
+++ js/script.js
@@ -49,18 +49,19 @@ const personalMovieDB = {
     }
   },
   writeYourGenres: function() {
-    for (let i = 1; i < 4; i++) {
-      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    for (let i = 1; i < 2; i++) {
+      let genres = prompt(`Введите ваши любимые жанры через запятую`);
 
-      if (genre === '' || genre == null) {
+      if (genres === '' || genres == null) {
         console.log('Вы ввели некорректные данные или не ввели их вообще');
         i--;
       } else {
-        personalMovieDB.genres[i - 1] = genre;
+        personalMovieDB.genres = genres.split(', ');
+        personalMovieDB.genres.sort();
       }
     }
     personalMovieDB.genres.forEach((item, i) => {
-      console.log(`Любимый жанр #${i + 1} - это ${item}`);
+      console.log(`Любимый жанр №${i + 1} - это ${item}`);
     });
   }
 
